f8f853d242f4006a4bbf93d1f35ae3265757e2e4
mdhilal9153 <mdhilal123098@gmail.com>
2026-03-29 21:11:00 +0530

    updated

--- frontend/src/components/LandingPage/InfoForm.jsx
+++ frontend/src/components/LandingPage/InfoForm.jsx
@@ -67,23 +67,23 @@ const InfoForm = () => {
 
         <p className='text-white font-semibold mt-3'>Experience Level</p>
         <div className='mt-2 flex justify-around'>
-            <button type='button' onClick={() => setBtn(1)} className={`border mr-2 py-2 px-10 rounded-xl transition-all bg-[#1b2029] ${btn == 1? 'border-[#00E5FF] text-[#00E5FF]': 'text-[#808080] border-[#808080]'}`} >Beginner</button>
-            <button type='button' onClick={() => setBtn(2)} className={`border mr-2 py-2 px-10 rounded-xl transition-all bg-[#1b2029] ${btn == 2? 'border-[#00E5FF] text-[#00E5FF]' : 'text-[#808080] border-[#808080]'}`}>Intermediate</button>
-            <button type='button' onClick={() => setBtn(3)} className={`border mr-2 py-2 px-10 rounded-xl transition-all bg-[#1b2029] ${btn == 3? 'border-[#00E5FF] text-[#00E5FF]' : 'text-[#808080] border-[#808080]'}`}>Advanced</button>
+            <button type='button' onClick={() => setBtn(1)} className={`border mr-2 py-2 px-10 rounded-xl transition-all bg-[#1b2029] hover:bg-[#282c36] ${btn == 1? 'border-[#00E5FF] text-[#00E5FF]': 'text-[#808080] border-[#808080]'}`} >Beginner</button>
+            <button type='button' onClick={() => setBtn(2)} className={`border mr-2 py-2 px-10 rounded-xl transition-all bg-[#1b2029] hover:bg-[#282c36] ${btn == 2? 'border-[#00E5FF] text-[#00E5FF]' : 'text-[#808080] border-[#808080]'}`}>Intermediate</button>
+            <button type='button' onClick={() => setBtn(3)} className={`border mr-2 py-2 px-10 rounded-xl transition-all bg-[#1b2029] hover:bg-[#282c36] ${btn == 3? 'border-[#00E5FF] text-[#00E5FF]' : 'text-[#808080] border-[#808080]'}`}>Advanced</button>
         </div>
 
         <p className='text-white font-semibold mt-3'>Interview Goal</p>
         <div className='mt-2 flex justify-around'>
 
 
-            <button type='button' onClick={() => arrC(0)} className={`border mr-2 py-2 px-6 rounded-full transition-all bg-[#1b2029] ${aim[0]? 'border-[#006400] text-[#006400]': 'text-[#808080] border-[#808080]'}`}>Land first job</button>
-            <button type='button' onClick={() => arrC(1)} className={`border mr-2 py-2 px-6 rounded-full transition-all bg-[#1b2029] ${aim[1]? 'border-[#006400] text-[#006400]': 'text-[#808080] border-[#808080]'}`}>Switch companies</button>
-            <button type='button' onClick={() => arrC(2)} className={`border mr-2 py-2 px-6 rounded-full transition-all bg-[#1b2029] ${aim[2]? 'border-[#006400] text-[#006400]': 'text-[#808080] border-[#808080]'}`}>Crack FAANG</button>
-            <button type='button' onClick={() => arrC(3)} className={`border mr-2 py-2 px-6 rounded-full transition-all bg-[#1b2029] ${aim[3]? 'border-[#006400] text-[#006400]': 'text-[#808080] border-[#808080]'}`}>Freelancing</button>
+            <button type='button' onClick={() => arrC(0)} className={`border mr-2 py-2 px-6 rounded-full transition-all bg-[#1b2029] hover:bg-[#282c36] ${aim[0]? 'border-[#00E676] text-[#00E676]': 'text-[#808080] border-[#808080]'}`}>Land first job</button>
+            <button type='button' onClick={() => arrC(1)} className={`border mr-2 py-2 px-6 rounded-full transition-all bg-[#1b2029] hover:bg-[#282c36] ${aim[1]? 'border-[#00E676] text-[#00E676]': 'text-[#808080] border-[#808080]'}`}>Switch companies</button>
+            <button type='button' onClick={() => arrC(2)} className={`border mr-2 py-2 px-6 rounded-full transition-all bg-[#1b2029] hover:bg-[#282c36] ${aim[2]? 'border-[#00E676] text-[#00E676]': 'text-[#808080] border-[#808080]'}`}>Crack FAANG</button>
+            <button type='button' onClick={() => arrC(3)} className={`border mr-2 py-2 px-6 rounded-full transition-all bg-[#1b2029] hover:bg-[#282c36] ${aim[3]? 'border-[#00E676] text-[#00E676]': 'text-[#808080] border-[#80808₀]'}`}>Freelancing</button>
         </div>
 
 
-        <button type='submit' className='bg-[#00FFFF] mt-5 w-full rounded-2xl py-1 flex justify-center items-center text-lg'>
+        <button type='submit' className='bg-[#00E5FF] text-[#0B0F19] mt-5 w-full rounded-2xl py-1 flex justify-center items-center text-lg'>
             Start Interview<ArrowRight/>
         </button>
 
